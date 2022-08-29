9b9c11e5097789c3ef40768a407367ab0e012135
lu1018 <112362763+lu1018@users.noreply.github.com>
2022-08-29 11:46:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_access_token():
     return access_token
  
  
-def get_weather(region):
+def get_weather(region1):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
@@ -59,7 +59,7 @@ def get_weather(region):
     # 风向
     wind_dir = response["now"]["windDir"]
     return weather, temp, wind_dir
- def get_weather(region):
+ def get_weather(region2):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
@@ -87,7 +87,7 @@ def get_weather(region):
     # 风向
     wind_dir = response["now"]["windDir"]
     return weather, temp, wind_dir
-   def get_weather(region):
+   def get_weather(region3):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
